4befccf46e0a521bf5dbaa263ec09bc57f343503
Brandon Haley <kyle7286@gmail.com>
2021-01-28 21:37:11 -0500

    Created searchButtonClicked function and listener

---
+++ logic/script.js
@@ -0,0 +1,15 @@
+// Global Variables
+// ......
+
+// Define Functons
+function searchButtonClicked() {
+    displayWeatherHistory();
+}
+
+// Display Weather History
+function displayWeatherHistory() {
+
+};
+
+// Event listener
+$("#searchButtonDashboard").click(searchButtonClicked);
