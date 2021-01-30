38dc02327b7c7590a4055507a6d15d2de53aed08
Brandon Haley <kyle7286@gmail.com>
2021-01-30 18:30:16 -0500

    Figured out AJX for historic week, but ran out of credits to query; saved the JSON object into local storage to continue coding with the dataset; commented out AJAX query for now but can use RESPONSE variable still

--- logic/script.js
+++ logic/script.js
@@ -4,13 +4,15 @@ var localTime = dateTime.local(); //Get the current local time
 
 // Define Functons
 function searchButtonClicked() {
-    // Grab user Input
-    var input = $("#searchedCityInput").val()
-    displayWeather(input);
+    // If input has value grab value, else do nothing
+    if ($("#searchedCityInput").val() !== "") {
+        var input = $("#searchedCityInput").val()
+        displayWeather(input);
+    }
 }
 
 // Display Weather which contains all the weather functions
-function displayWeather(city) {
+function displayWeather(location) {
     // Get current local date
     var currentDate = dateTime.local();
     var currentDateISO = dateTime.local().toISODate();
@@ -47,22 +49,44 @@ function displayWeather(city) {
         console.log(oneYearAgoFormatted + "\n" + oneYearAgoWeekFormatted + "\n" + oneYearAgoMonthStartFormatted + "\n" + oneYearAgoMonthEndFormatted + "\n" + oneYearAgoTwoMonthsStartFormatted + "\n" + oneYearAgoTwoMonthsEndFormatted);
 
 
-        // apiKey = "Q5Z5S9QT8FD8UJKCGYBURUXX8"
-        // // var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY"
-        // //              https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY 
-        // var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sugar%20hill%20ga/" + oneYearAgo + "/" + oneYearAgoWeek + "?unitGroup=us&key=Q5Z5S9QT8FD8UJKCGYBURUXX8"
-        // console.log(queryURL);
-        // //              https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sugar%20hill%20ga/2020-1-30/2020-1-31?unitGroup=us&key=Q5Z5S9QT8FD8UJKCGYBURUXX8
-        // //              https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sugar%20hill%20ga/2020-01-30/2020-02-06?unitGroup=us&key=Q5Z5S9QT8FD8UJKCGYBURUXX8
-
-        // $.ajax({
-        //     url: queryURL,
-        //     method: 'GET',
-        // }).then(function (response) {
-        //     console.log(response);
-        // });
+        getHistoricWeek();
+        // getHistoricOneMonth();
 
 
+
+        // Returns JSON object of a year ago, 1 week. 
+        // The API KEY in this ajax call should be used for 
+        function getHistoricWeek() {
+            var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "/" + oneYearAgoFormatted + "/" + oneYearAgoWeekFormatted + "?unitGroup=us&key=Q5Z5S9QT8FD8UJKCGYBURUXX8&include=obs"
+            var response = JSON.parse(localStorage.getItem("historic-week"))
+            console.log(response);
+
+
+            // Commenting out AJAX requesting while developing; uncomment logic before pushing to prod
+            // $.ajax({
+            //     url: queryURL,
+            //     method: 'GET',
+            // }).then(function (response) {
+            //     console.log(response);
+
+            //     // Save the object in history to prevent from using query credits and running out
+            //     // localStorage.setItem('historic-week', JSON.stringify(response));
+
+            // Grab Data from JSON - Leave this line commented
+            
+            
+
+
+
+
+
+            // });
+        }
+
+
+        function getHistoricOneMonth() {
+
+        }
 
 
     }
