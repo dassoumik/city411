998782cc7aaa8c5832a3ec6dbef96b2462dcb24a
Brandon Haley <kyle7286@gmail.com>
2021-01-31 22:30:19 -0500

    completed historic data with AJAX verfied working for all historic calls; saved objects to local storage in this commit for current & nexxt month JSON data

--- logic/script.js
+++ logic/script.js
@@ -27,6 +27,7 @@ function displayWeather(location) {
     displayHistoric();
 
 
+
     // Historic Data
     function displayHistoric() {
         // Define the date ranges to use in query and create correct syntax string for query
@@ -53,172 +54,177 @@ function displayWeather(location) {
 
         // Test Variables
         // console.log(oneYearAgoFormatted + "\n" + oneYearAgoWeekFormatted + "\n" + oneYearAgoMonthStartFormatted + "\n" + oneYearAgoMonthEndFormatted + "\n" + oneYearAgoTwoMonthsStartFormatted + "\n" + oneYearAgoTwoMonthsEndFormatted);
+        getHistoricWeek("Q5Z5S9QT8FD8UJKCGYBURUXX8");
+        getHistoricCurrentMonth("current", "347KV25P3E7B8XKZMTG2ETSNJ", oneYearAgoMonthStartFormatted, oneYearAgoMonthEndFormatted);
+        getHistoricCurrentMonth("next", "EZAX7WB9Q7WQZZ6582Q64AVZH", oneYearAgoTwoMonthsStartFormatted, oneYearAgoTwoMonthsEndFormatted);
 
-
-        getHistoricWeek();
-        getHistoricCurrentMonth();
 
         // Returns JSON object of a year ago, 1 week. 
-        // The API KEY in this ajax call should be used for 
-        function getHistoricWeek() {
+        function getHistoricWeek(key) {
             // Empty the div to make room for JS created HTML
             $("#historic-week-div").empty();
 
-            var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "/" + oneYearAgoFormatted + "/" + oneYearAgoWeekFormatted + "?unitGroup=us&key=Q5Z5S9QT8FD8UJKCGYBURUXX8&include=obs"
+            var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "/" + oneYearAgoFormatted + "/" + oneYearAgoWeekFormatted + "?unitGroup=us&key=" + key + "&include=obs"
             // Grab Data from JSON - Leave this line commented
-            var response = JSON.parse(localStorage.getItem("historic-week"))
-            console.log(response);
+            // var response = JSON.parse(localStorage.getItem("historic-week"))
+            // console.log(response);
 
             // Commenting out AJAX requesting while developing; uncomment logic before pushing to prod
-            // $.ajax({
-            //     url: queryURL,
-            //     method: 'GET',
-            // }).then(function (response) {
-            //     console.log(response);
+            $.ajax({
+                url: queryURL,
+                method: 'GET',
+            }).then(function (response) {
+                console.log(response);
 
-            // Save the object in history to prevent from using query credits and running out
-            // localStorage.setItem('historic-week', JSON.stringify(response));
+                // Save the object in history to prevent from using query credits and running out
+                // localStorage.setItem('historic-week', JSON.stringify(response));
 
-            console.log("=========================");
-            // City/state
-            var address = response.resolvedAddress
-            // Array data
-            var aDayRange = response.days
-            console.log(address);
-            console.log(aDayRange);
+                // City/state
+                var address = response.resolvedAddress
 
-            // Loop thru each Historic Week day...
-            $.each(aDayRange, function (index, item) {
+                // Array data
+                var aDayRange = response.days
 
-                var historicDay = aDayRange[index].datetime;
-                var historicDayEpoch = aDayRange[index].datetimeEpoch;
-                var historicDayConditions = aDayRange[index].conditions;
-                var historicDayHumidity = aDayRange[index].humidity;
-                var historicDayIcon = aDayRange[index].icon;
-                var historicDayPrecip = aDayRange[index].precip;
-                var historicDaySnow = aDayRange[index].snow;
-                var historicDayTempmax = Math.round(aDayRange[index].tempmax);
-                var historicDayTempmin = Math.round(aDayRange[index].tempmin);
-                var historicDayWindspeed = Math.round(aDayRange[index].windspeed);
+                // Loop thru each Historic Week day...
+                $.each(aDayRange, function (index, item) {
 
-                // Create elements with the data
-                var dayDiv = $("<div>").attr("id", "historic-day-" + index).attr("class", "tile is-child is-vertical notification is-info p-1");
-                var dayTitle = $("<div>").attr("class", "myBold").text(dateTime.fromSeconds(historicDayEpoch).weekdayShort);
+                    var historicDay = aDayRange[index].datetime;
+                    var historicDayEpoch = aDayRange[index].datetimeEpoch;
+                    var historicDayConditions = aDayRange[index].conditions;
+                    var historicDayHumidity = aDayRange[index].humidity;
+                    var historicDayIcon = aDayRange[index].icon;
+                    var historicDayPrecip = aDayRange[index].precip;
+                    var historicDaySnow = aDayRange[index].snow;
+                    var historicDayTempmax = Math.round(aDayRange[index].tempmax);
+                    var historicDayTempmin = Math.round(aDayRange[index].tempmin);
+                    var historicDayWindspeed = Math.round(aDayRange[index].windspeed);
 
-                // Create ICON element based on ICON TEXT variable from response
-                if (historicDayIcon === "rain") { var dayIcon = $("<i>").attr("class", "fas fa-cloud-showers-heavy my-2 is-size-4"); }
-                else if (historicDayIcon === "fog") { var dayIcon = $("<i>").attr("class", "fas fa-smog my-2 is-size-4"); }
-                else if (historicDayIcon === "wind") { var dayIcon = $("<i>").attr("class", "fas fa-wind my-2 is-size-4"); }
-                else if (historicDayIcon === "cloudy") { var dayIcon = $("<i>").attr("class", "fas fa-cloud my-2 is-size-4"); }
-                else if (historicDayIcon === "partly-cloudy-day") { var dayIcon = $("<i>").attr("class", "fas fa-cloud-sun my-2 is-size-4"); }
-                else if (historicDayIcon === "partly-cloudy-night") { var dayIcon = $("<i>").attr("class", "fas fa-cloud-moon my-2 is-size-4"); }
-                else if (historicDayIcon === "clear-day") { var dayIcon = $("<i>").attr("class", "fas fa-sun my-2 is-size-4"); }
-                else if (historicDayIcon === "clear-night") { var dayIcon = $("<i>").attr("class", "fas fa-moon my-2 is-size-4"); }
-                else { var dayIcon = $("<i>").attr("class", "fas fa-question my-2 is-size-4"); }
+                    // Create elements with the data
+                    var dayDiv = $("<div>").attr("id", "historic-day-" + index).attr("class", "tile is-child is-vertical notification is-info p-1");
+                    var dayTitle = $("<div>").attr("class", "myBold").text(dateTime.fromSeconds(historicDayEpoch).weekdayShort);
 
-                var dayHighDiv = $("<div>").text("Hi: " + historicDayTempmax + "°F");
-                var dayLowDiv = $("<div>").text("Lo: " + historicDayTempmin + "°F");
-                var dayIconText = $("<div>").text(historicDayIcon);
+                    // Create ICON element based on ICON TEXT variable from response
+                    if (historicDayIcon === "rain") { var dayIcon = $("<i>").attr("class", "fas fa-cloud-showers-heavy my-2 is-size-4"); }
+                    else if (historicDayIcon === "fog") { var dayIcon = $("<i>").attr("class", "fas fa-smog my-2 is-size-4"); }
+                    else if (historicDayIcon === "wind") { var dayIcon = $("<i>").attr("class", "fas fa-wind my-2 is-size-4"); }
+                    else if (historicDayIcon === "cloudy") { var dayIcon = $("<i>").attr("class", "fas fa-cloud my-2 is-size-4"); }
+                    else if (historicDayIcon === "partly-cloudy-day") { var dayIcon = $("<i>").attr("class", "fas fa-cloud-sun my-2 is-size-4"); }
+                    else if (historicDayIcon === "partly-cloudy-night") { var dayIcon = $("<i>").attr("class", "fas fa-cloud-moon my-2 is-size-4"); }
+                    else if (historicDayIcon === "clear-day") { var dayIcon = $("<i>").attr("class", "fas fa-sun my-2 is-size-4"); }
+                    else if (historicDayIcon === "clear-night") { var dayIcon = $("<i>").attr("class", "fas fa-moon my-2 is-size-4"); }
+                    else { var dayIcon = $("<i>").attr("class", "fas fa-question my-2 is-size-4"); }
 
-                // Append all elements to daydiv then to tile div
-                dayDiv.append(dayTitle, dayIcon, dayHighDiv, dayLowDiv)
-                $("#historic-week-div").append(dayDiv);
+                    var dayHighDiv = $("<div>").text("Hi: " + historicDayTempmax + "°F");
+                    var dayLowDiv = $("<div>").text("Lo: " + historicDayTempmin + "°F");
+                    var dayIconText = $("<div>").text(historicDayIcon);
 
-            });
+                    // Append all elements to daydiv then to tile div
+                    dayDiv.append(dayTitle, dayIcon, dayHighDiv, dayLowDiv)
+                    $("#historic-week-div").append(dayDiv);
+
+                });
 
 
-            // }); // Uncomment this for prod
+            }); // Uncomment this for prod
         }
 
 
-        function getHistoricCurrentMonth() {
-            var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "/" + oneYearAgoMonthStartFormatted + "/" + oneYearAgoMonthEndFormatted + "?unitGroup=us&key=347KV25P3E7B8XKZMTG2ETSNJ&include=obs"
+        function getHistoricCurrentMonth(month, key, start, end) {
+            var queryURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "/" + start + "/" + end + "?unitGroup=us&key=" + key + "&include=obs"
             // Grab Data from JSON - Leave this line commented
-            var response = JSON.parse(localStorage.getItem("historic-current-month"));
-            console.log(response);
+            // var response = JSON.parse(localStorage.getItem(month));
+            // console.log(response);
 
             // Commenting out AJAX requesting while developing; uncomment logic before pushing to prod
-            // $.ajax({
-            //     url: queryURL,
-            //     method: 'GET',
-            // }).then(function (response) {
-            //     console.log(response);
+            $.ajax({
+                url: queryURL,
+                method: 'GET',
+            }).then(function (response) {
+                console.log(response);
 
-            // Save the object in history to prevent from using query credits and running out
-            // localStorage.setItem('historic-current-month', JSON.stringify(response));
-            console.log("=========================");
-            // Array data / Outer Loop variables
-            var aDayRange = response.days
-            console.log(aDayRange);
+                // Save the object in history to prevent from using query credits and running out
+                localStorage.setItem(month, JSON.stringify(response));
 
-            // Declare Dynamic Variable
-            var totalHi = 0;
-            var totalLo = 0;
-            var aCond = [];
-            var aConditionsConcat = [];
-            var aConditionsUsed = [];
-            var Obj = {};
+                // Array data / Outer Loop variables
+                var aDayRange = response.days
 
-            // Loop thru each Historic Week day...
-            // Build the strings USED & CONCAT string
-            $.each(aDayRange, function (index, item) {
-                totalHi = totalHi + parseInt(item.tempmax);
-                totalLo = totalLo + parseInt(item.tempmin);
 
-                // Get all current conditions, tally them up
-                var aCurrentConditions = aDayRange[index].conditions.split(",");
-                // Build Used String
-                for (let i = 0; i < aCurrentConditions.length; i++) {
-                    var currentCondition = (aCurrentConditions[i].trim(" ")).toLowerCase();
-                    if (aConditionsUsed.indexOf(currentCondition) === -1) {
-                        aConditionsUsed.push(currentCondition);
+                // Declare Dynamic Variable
+                var totalHi = 0;
+                var totalLo = 0;
+                var aCond = [];
+                var aConditionsConcat = [];
+                var aConditionsUsed = [];
+                var Obj = {};
+
+                // Loop thru each Historic Week day...
+                // Build the strings USED & CONCAT string
+                $.each(aDayRange, function (index, item) {
+                    totalHi = totalHi + parseInt(item.tempmax);
+                    totalLo = totalLo + parseInt(item.tempmin);
+
+                    // Get all current conditions, tally them up
+                    var aCurrentConditions = aDayRange[index].conditions.split(",");
+                    // Build Used String
+                    for (let i = 0; i < aCurrentConditions.length; i++) {
+                        var currentCondition = (aCurrentConditions[i].trim(" ")).toLowerCase();
+                        if (aConditionsUsed.indexOf(currentCondition) === -1) {
+                            aConditionsUsed.push(currentCondition);
+                        }
+
+                        // Build Concat String (for latter count)
+                        aConditionsConcat.push(currentCondition);
                     }
+                });
 
-                    // Build Concat String (for latter count)
-                    aConditionsConcat.push(currentCondition);
+                // Grab file concat String
+                let sFinalConcat = aConditionsConcat.join("");
+
+                // Get condtion tallies
+                // Loop thru used array, .match to finalstring, write to Obj property & value
+                for (let i = 0; i < aConditionsUsed.length; i++) {
+                    var re = new RegExp(aConditionsUsed[i], "g");
+                    Obj[aConditionsUsed[i]] = sFinalConcat.match(re).length;
                 }
-            });
+                console.log(Obj);
 
-            // Grab file concat String
-            let sFinalConcat = aConditionsConcat.join("");
-
-            // Get condtion tallies
-            // Loop thru used array, .match to finalstring, write to Obj property & value
-            for (let i = 0; i < aConditionsUsed.length; i++) {
-                var re = new RegExp(aConditionsUsed[i], "g");
-                Obj[aConditionsUsed[i]] = sFinalConcat.match(re).length;
-            }
-            console.log(Obj);
-
-            // Push object into array and sort descending to grab first 
-            var sortable = [];
-            for (var condition in Obj) {
-                sortable.push([condition, Obj[condition]]);
-            }
-            sortable.sort(function (a, b) {
-                return b[1] - a[1];
-            });
+                // Push object into array and sort descending to grab first 
+                var sortable = [];
+                for (var condition in Obj) {
+                    sortable.push([condition, Obj[condition]]);
+                }
+                sortable.sort(function (a, b) {
+                    return b[1] - a[1];
+                });
 
 
-            // Final Values to display:
-            var mostlyCondition = sortable[0][0];
-            var avgHi = Math.round((totalHi / aDayRange.length));
-            var avgLo = Math.round((totalLo / aDayRange.length));
+                // Final Values to display:
+                var mostlyCondition = sortable[0][0];
+                var avgHi = Math.round((totalHi / aDayRange.length));
+                var avgLo = Math.round((totalLo / aDayRange.length));
 
-            console.log(sortable[0][0]);
-            console.log("AvgHi: " + avgHi);
-            console.log("AvgLo: " + avgLo);
+                console.log(sortable[0][0]);
+                console.log("AvgHi: " + avgHi);
+                console.log("AvgLo: " + avgLo);
 
-            // Create elements
-            var hcmTitleDiv = $("<div>").attr("class", "is-size-6 myBold").text(oneYearAgo.monthLong);
-            var mostlyCondDiv = $("<div>").text("Mostly " + mostlyCondition);
-            var avgHiDiv = $("<div>").text("Hi average - " + avgHi);
-            var avgLoDiv = $("<div>").text("Lo average - " + avgLo);
-            $("#historic-current-month").empty();
-            $("#historic-current-month").append(hcmTitleDiv, mostlyCondDiv, avgHiDiv, avgLoDiv)
+                // Clear previous Month Elements:
+                $("#" + month).empty();
+
+                // Create elements
+                if (month === "current") {
+                    var hcmTitleDiv = $("<div>").attr("class", "is-size-6 myBold").text(oneYearAgo.monthLong);
+                }
+                else if (month === "next") { var hcmTitleDiv = $("<div>").attr("class", "is-size-6 myBold").text(oneYearAgoTwoMonthsStart.monthLong); }
+                else if (month === "nextnext") { var hcmTitleDiv = $("<div>").attr("class", "is-size-6 myBold").text(oneYearAgo.monthLong); }
+                var mostlyCondDiv = $("<div>").text("Mostly " + mostlyCondition);
+                var avgHiDiv = $("<div>").text("Hi average - " + avgHi);
+                var avgLoDiv = $("<div>").text("Lo average - " + avgLo);
+
+                // Append the above elements
+                $("#" + month).append(hcmTitleDiv, mostlyCondDiv, avgHiDiv, avgLoDiv)
 
 
-            // }); // Uncomment this for prod
+            }); // Uncomment this for prod
         }
 
 
