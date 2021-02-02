eec17bb9511542582ae5e45c7c98f543d001a283
wryanj <ryanjohnson9685@gmail.com>
2021-02-02 18:06:44 -0500

    MVP ui updates

--- logic/uiEffects.js
+++ logic/uiEffects.js
@@ -3,3 +3,27 @@
     // Enter submits value on form inputs...
 
     // Menu collapses in hamburger on smaller viewports
+
+// Media Query to Remove Clases ON Viewport Change
+    
+    
+    // This evens out the weather tiles on mobile phones and tablets
+        if (matchMedia) {
+            console.log("event listner for screen width fired")
+            var mobileBreakPoint = window.matchMedia( "(max-width:768px)");
+            mobileBreakPoint.addEventListener;
+            makeMobile(mobileBreakPoint);
+        }
+
+        function makeMobile(mobileBreakPoint) {
+            if (mobileBreakPoint.matches) {
+                console.log("making for mobile");
+                $("#historicLargeTile").removeClass("py-0 mx-1");
+                $("#historicLargeTile").addClass("p-0");
+            }
+            else {
+                console.log("making for regular");
+                $("#historicLargeTile").addClass("py-0 mx-1");
+                $("#historicLargeTile").removeClass("p-0");
+            }
+        }
